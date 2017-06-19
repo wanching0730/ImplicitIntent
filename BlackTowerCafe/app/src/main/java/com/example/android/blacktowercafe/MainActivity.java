2da347709a035863d3b8f278b02661506c0c38ad
Wan Ching <wanching_97@hotmail.com>
2017-06-19 22:54:53 +0800

    git add

---
+++ BlackTowerCafe/app/src/main/java/com/example/android/blacktowercafe/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.blacktowercafe;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mapButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapButton = (Button) findViewById(R.id.google_map);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:3.034737, 101.767109"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
